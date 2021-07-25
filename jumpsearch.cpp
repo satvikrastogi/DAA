532a26da74dbbbc4b8a76d96f696803363be1607
satvikrastogi <69415639+satvikrastogi@users.noreply.github.com>
2021-07-25 22:43:28 +0530

    Update jumpsearch.cpp

--- jumpsearch.cpp
+++ jumpsearch.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+  int t;
+  cout<<"Enter test case=";
+  cin>>t;
+  for(int i=0;i<t;i++){
+  int n;
+  cout<<"Enter the limit of array=";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements of array:";
+  for(int j=0;j<n;j++)
+    cin>>arr[j];
+  int k,count=1;
+  cout<<"Enter searching element=";
+   cin>>k;
+  int limit=sqrt(n);
+  int start=0;
+  int end=limit;
+  while(arr[end]<k){
+     start=end;
+     end=end+limit;
+     count++;
+     if(end>n-1){
+        end=n-1;
+        break;
+        }
+     }
+     int l;
+  for(l=start+1;l<=end;l++)
+    {
+        count++;
+    if(arr[l]==k){
+      cout<<"Present\nNumber of count="<<count<<endl;
+      break;
+      }
+      }
+  if(l>end)
+    cout<<"Not present"<<endl;
+    }
+    return 0;
+    }
 
